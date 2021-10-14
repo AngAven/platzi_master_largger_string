bca4c1187768e5e19f7abc6044589391fdb1b6cb
Angel <angel.aven.c@gmail.com>
2021-10-14 13:59:31 -0500

    Lógica base
    Comparación de longitud del arreglo actual contra el anterior que haya sido el mayor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './styles/index.css'
 
-console.log('inicio')
+const maxArray = () => {
+  const fullArray = document
+    .querySelector('.full-array')
+    .value
+    .trim()
+    .split(' ')
+
+  let arrayMaxLengthPosition = 0
+
+  fullArray.forEach((element, index, array) => {
+    if (index > 0 ){
+      const actualElementLength = element.length
+
+      // Comparación entre el elemento actual, el que tuvo la cadena mas larga anteriormente
+      if (actualElementLength > array[arrayMaxLengthPosition].length){
+        arrayMaxLengthPosition = index
+      }
+
+      return
+    }
+
+    arrayMaxLengthPosition = index
+  })
+
+  console.log('El elemento con mayor número de caracteres es el: ',  fullArray[arrayMaxLengthPosition])
+}
+
+export default maxArray
